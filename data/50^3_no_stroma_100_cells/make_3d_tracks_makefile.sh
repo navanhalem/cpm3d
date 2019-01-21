a2a00ec1e8c4289f12bab40ed2c37f6524c14d61
Nino van Halem <na.vanhalem@gmail.com>
2019-01-21 14:26:08 +0100

    organised makefiles, made new makefile for 50x50x50 and 100 cells simulations

---
+++ data/50^3_no_stroma_100_cells/make_3d_tracks_makefile.sh
@@ -0,0 +1,36 @@
+settings=$settingsfolder/$expname-settings.js
+template=$settingsfolder/$expname-template.js #txt
+
+paramfile=params.txt
+nsim=1
+
+# ---------------------------------------------------------------------
+# CODE:
+# ---------------------------------------------------------------------
+
+np=$( cat $paramfile | wc -l )
+
+echo ".DELETE_ON_ERROR :"
+echo "all : "
+
+# Loop over the simulations and parameter combis
+for p in $(seq 1 $np) ; do
+
+	LFORCED=$( cat $paramfile | awk -v line=$p 'NR==line{print $1}')
+	LRAND=$( cat $paramfile | awk -v line=$p 'NR==line{print $2}')
+	LDIR=$( cat $paramfile | awk -v line=$p 'NR==line{print $3}')
+	STROMA=$( cat $paramfile | awk -v line=$p 'NR==line{print $4}')
+	EXPNAME="3d"
+		
+	NAME=$EXPNAME-lforced$LFORCED-lrand$LRAND-ldir$LDIR-stroma$STROMA
+
+	# Now the recipes for the individual simulation tracks
+	for sim in $(seq 1 $nsim) ; do
+
+		# trackfiles
+		FILE=data/$NAME-sim$sim.txt
+		echo "$FILE : ../../cpm3d.js"
+		echo -e "\t@"node \$\< $LFORCED $LRAND $LDIR 100 50 2000 $STROMA "> \$@"
+		echo "all : "$FILE
+	done
+done
